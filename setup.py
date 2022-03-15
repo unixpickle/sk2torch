f8e13dd9a186fdccfe5cdf94e9aa1ed6ce30efca
Alex Nichol <unixpickle@gmail.com>
2022-03-15 13:19:02 -0700

    bump version, push to pypi

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="sk2torch",
-    version="1.0.0",
+    version="1.1.0",
     description="Convert scikit-learn models to PyTorch modules",
     long_description="Convert scikit-learn models to PyTorch modules",
     packages=["sk2torch"],
